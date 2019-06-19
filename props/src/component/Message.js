1facc1e947de42c12a00c1e894051093a7d47d83
Melissa Stock <melissastock44@gmail.com>
2019-06-19 13:08:39 -0700

    Added message component and rendered to page

---
+++ props/src/component/Message.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Message extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      text: "Hello World",
+    };
+
+  }
+  render(){
+    return <div>{ this.state.text }</div>;
+  }
+}
--- props/src/index.js
+++ props/src/index.js
@@ -2,12 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './styles.css';
+import Message from './component/Message.js';
+
+// class Message extends React.Component{
+//   constructor(props){
+//     super(props);
+//     this.state = {
+//       text: "Hello World",
+//     };
+
+//   }
+//   render(){
+//     return <div>{ this.state.text }</div>;
+//   }
+// }
 
 class App extends React.Component {
   render() {
-    return <div />;
+    return <Message />;
   }
 }
+
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
